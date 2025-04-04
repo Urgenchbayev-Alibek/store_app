aa5bac103c5a36e359ec2d0496f680b981e2b630
Alibek <urgenchbaevalibek@gmail.com>
2025-04-04 18:37:11 +0500

    some changes

---
+++ lib/core/routing/router.dart
@@ -0,0 +1,4 @@
+import 'package:go_router/go_router.dart';
+import 'package:store_app/core/routing/routes.dart';
+
+final GoRouter router = GoRouter(initialLocation: Routes.splashScreen, routes: []);
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,49 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(StoreApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  runApp(const MyApp());
 }
 
-class StoreApp extends StatelessWidget {
-  const StoreApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreenView(),
+    );
+  }
+}
+
+class SplashScreenView extends StatelessWidget {
+  const SplashScreenView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Image.asset(
+            "assets/images/splash.png",
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            bottom: 60,
+            child: SizedBox(
+              width: 64,
+              height: 64,
+              child: CircularProgressIndicator(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
