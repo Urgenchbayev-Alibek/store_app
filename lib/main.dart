b298561d4e7c15a96906bbc3f5f6ebd27d760654
Alibek <urgenchbaevalibek@gmail.com>
2025-04-04 19:31:58 +0500

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:store_app/core/routing/router.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-  runApp(const MyApp());
+  runApp(const StoreApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class StoreApp extends StatelessWidget {
+  const StoreApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      home: const SplashScreenView(),
+      routerConfig: router,
     );
   }
 }
 
-class SplashScreenView extends StatelessWidget {
-  const SplashScreenView({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Image.asset(
-            "assets/images/splash.png",
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-          Positioned(
-            bottom: 60,
-            child: SizedBox(
-              width: 64,
-              height: 64,
-              child: CircularProgressIndicator(color: Colors.white),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+
+
+
