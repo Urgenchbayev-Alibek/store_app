085e17601f2ac5250a3cc3098a39467c8099b738
Alibek <urgenchbaevalibek@gmail.com>
2025-04-17 19:31:37 +0500

    help center

--- lib/features/help_center/pages/help_center_view.dart
+++ lib/features/help_center/pages/help_center_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
 import 'package:store_app/core/sizes.dart';
-import 'package:store_app/core/utils/colors.dart';
+import 'package:store_app/features/common/bottom_nav_bar.dart';
 import 'package:store_app/features/common/store_app_bar.dart';
+
+import '../../../core/routing/routes.dart';
+import '../widgets/help_center_item.dart';
 
 class HelpCenterView extends StatelessWidget {
   const HelpCenterView({super.key});
@@ -11,63 +13,50 @@ class HelpCenterView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: StoreAppBar(title: "Help Center"),
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: AppSizes.padding25),
-          child: Column(
-            children: [
-              Divider(color: Colors.grey.withValues(alpha: 0.45)),
-              SizedBox(height: 15),
-              HelpCenterItem(icon: "assets/icons/headphones.svg", text: "Customer Service"),
-              SizedBox(height: 15),
-              HelpCenterItem(icon: "assets/icons/whatsapp.svg", text: "Whatsapp"),
-              SizedBox(height: 15),
-              HelpCenterItem(icon: "assets/icons/website.svg", text: "Website"),
-              SizedBox(height: 15),
-              HelpCenterItem(icon: "assets/icons/facebook.svg", text: "Facebook"),
-              SizedBox(height: 15),
-              HelpCenterItem(icon: "assets/icons/twitter.svg", text: "Twitter"),
-              SizedBox(height: 15),
-              HelpCenterItem(icon: "assets/icons/instagram.svg", text: "Instagram"),
-            ],
-          ),
-        ));
-  }
-}
-
-class HelpCenterItem extends StatelessWidget {
-  const HelpCenterItem({super.key, required this.icon, required this.text});
-
-  final String icon, text;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
+      appBar: StoreAppBar(title: "Help Center"),
+      body: Padding(
         padding: EdgeInsets.symmetric(horizontal: AppSizes.padding25),
-        width: 341.w,
-        height: 52.h,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(
-            color: Colors.grey.withOpacity(0.2),
-          ),
-        ),
-        child: Row(
+        child: Column(
           children: [
-            SvgPicture.asset(icon),
-            SizedBox(width: 10),
-            Text(
-              text,
-              style: TextStyle(
-                color: AppColors.primary,
-                fontFamily: "General Sans",
-                fontSize: 16,
-              ),
-            )
+            Divider(color: Colors.grey.withValues(alpha: 0.45)),
+            SizedBox(height: 15),
+            HelpCenterItem(icon: "assets/icons/headphones.svg", text: "Customer Service"),
+            SizedBox(height: 15),
+            HelpCenterItem(icon: "assets/icons/whatsapp.svg", text: "Whatsapp"),
+            SizedBox(height: 15),
+            HelpCenterItem(icon: "assets/icons/website.svg", text: "Website"),
+            SizedBox(height: 15),
+            HelpCenterItem(icon: "assets/icons/facebook.svg", text: "Facebook"),
+            SizedBox(height: 15),
+            HelpCenterItem(icon: "assets/icons/twitter.svg", text: "Twitter"),
+            SizedBox(height: 15),
+            HelpCenterItem(icon: "assets/icons/instagram.svg", text: "Instagram"),
           ],
         ),
+      ),
+      bottomNavigationBar: StoreBottomNavigationBar(
+        selectedIndex: 3,
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              context.push(Routes.home);
+              break;
+            case 1:
+              context.push(Routes.search);
+              break;
+            case 2:
+              context.push(Routes.saved);
+              break;
+            case 3:
+              context.push(Routes.myCart);
+              break;
+            case 4:
+              context.push(Routes.account);
+              break;
+            default:
+              break;
+          }
+        },
       ),
     );
   }
